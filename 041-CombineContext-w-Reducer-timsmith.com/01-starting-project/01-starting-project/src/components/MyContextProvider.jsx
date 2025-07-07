20d72e1cd632b132ae298ec745a771b44572401e
Carlos-Felipe Rodriguez <feliperod0519@hotmail.com>
2025-07-07 19:33:23 -0400

    reduce and context fix

--- 041-CombineContext-w-Reducer-timsmith.com/01-starting-project/01-starting-project/src/components/MyContextProvider.jsx
+++ 041-CombineContext-w-Reducer-timsmith.com/01-starting-project/01-starting-project/src/components/MyContextProvider.jsx
@@ -5,12 +5,10 @@ import { myReducer } from "./MyReducer";
 //Step 2: Create Provider to wrap it up
 //Step 4: Replace state in context with Reducer
 
-const initialState = { name:'minou'}
-
 export const MyContextProvider = ({children}) =>{
 
     //const [name,setName] = useState(""); //step2
-    const [state,dispatch] = useReducer(myReducer,initialState); //step4
+    const [state,dispatch] = useReducer(myReducer,{ name:'minou'}); //step4
 
     return (
         <MyContext.Provider value={{ state, dispatch }}>
